6d556bb19ec55793bf49c9e5689dbcfc5647dea2
AmletixQ <nazarovamridin308@gmail.com>
2026-04-12 16:28:01 +0300

    Initialization

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => c.text("Hello!"));
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
