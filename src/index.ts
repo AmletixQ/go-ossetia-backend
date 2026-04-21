8d797ff744d86c6bec3cba08dfaf065d8a71398a
AmletixQ <nazarovamridin308@gmail.com>
2026-04-21 22:54:30 +0300

    feat: создал основные модели для БД, а также клиент Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { prisma } from "./lib/prisma";
 
 const app = new Hono();
 
-app.get("/", (c) => c.text("Hello!"));
+app.get("/", async (c) => {
+  const res = await prisma.event.findMany();
+
+  return c.json({ events: res });
+});
 
 serve(
   {
---
+++ src/lib/prisma.ts
@@ -0,0 +1,21 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+import * as dotenv from "dotenv";
+import * as dotenvExpand from "dotenv-expand";
+
+dotenvExpand.expand(dotenv.config());
+
+const databaseUrl = process.env.DATABASE_URL;
+if (!databaseUrl)
+  throw new Error("DATABASE_URL is not defined in environment variables");
+
+console.log(databaseUrl);
+
+const adapter = new PrismaPg({
+  connectionString: databaseUrl,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
