7605e532430bec03c89213147f7a5955bdc4920c
AmletixQ <nazarovamridin308@gmail.com>
2026-04-25 21:42:42 +0300

    feat: создал middleware для проверки доступа

---
+++ src/middlewares/owner-check.ts
@@ -0,0 +1,22 @@
+import { Context, Next } from "hono";
+import { Role } from "../generated/prisma/enums";
+import { ResponseFactory } from "../utils/response-factory";
+
+export function checkResourceOwnership(
+  getResourceOwnerId: (ctx: Context) => Promise<string | undefined> | string,
+) {
+  return async (ctx: Context, next: Next) => {
+    const userId = ctx.get("userId");
+    const userRole = ctx.get("userRole") as Role;
+    const resourceOwnerId = await getResourceOwnerId(ctx);
+
+    if (userRole === "ADMIN" || userId === resourceOwnerId) {
+      return await next();
+    }
+
+    return ResponseFactory.forbidden(
+      ctx,
+      "Доступ запрещен: вы не являетесь владельцем этого ресурса",
+    );
+  };
+}
