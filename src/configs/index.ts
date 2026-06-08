d69e4594a39f32eb1e4201e891de7c54f8bd2630
AmletixQ <nazarovamridin308@gmail.com>
2026-06-08 17:29:25 +0300

    feat: создал небольшое приветственное сообщение и ссылку на документацию

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,6 @@
 import { AUTH_CONFIG } from "./auth.config";
 import { EMAIL_TEMPLATES } from "./email.config";
 import { OPEN_API_CONFIG } from "./open-api/open-api.config";
+import { HOME_TEMPLATE } from "./home.template";
 
-export { AUTH_CONFIG, EMAIL_TEMPLATES, OPEN_API_CONFIG };
+export { AUTH_CONFIG, EMAIL_TEMPLATES, OPEN_API_CONFIG, HOME_TEMPLATE };
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { logger } from "hono/logger";
 
 import { auth, events } from "./routes";
 import { errorHandler } from "./middlewares";
-import { OPEN_API_CONFIG } from "./configs";
+import { HOME_TEMPLATE, OPEN_API_CONFIG } from "./configs";
 
 const app = new Hono();
 app.basePath("/api");
@@ -13,6 +13,7 @@ app.basePath("/api");
 app.use(logger());
 app.onError(errorHandler);
 
+app.get("/", (ctx) => ctx.html(HOME_TEMPLATE));
 app.get("/doc", (ctx) => ctx.json(OPEN_API_CONFIG));
 app.get("/open-api", swaggerUI({ title: "Go Ossetia API", url: "/doc" }));
 
