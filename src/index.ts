975cfb04d1f49db034d649147423a5feb104a84d
AmletixQ <nazarovamridin308@gmail.com>
2026-06-08 17:28:50 +0300

    feat: установил и подключил к документации `Open API` `Swagger UI`

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import { serve } from "@hono/node-server";
+import { swaggerUI } from "@hono/swagger-ui";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
 import { auth, events } from "./routes";
 import { errorHandler } from "./middlewares";
+import { OPEN_API_CONFIG } from "./configs";
 
 const app = new Hono();
+app.basePath("/api");
 
 app.use(logger());
 app.onError(errorHandler);
+
+app.get("/doc", (ctx) => ctx.json(OPEN_API_CONFIG));
+app.get("/open-api", swaggerUI({ title: "Go Ossetia API", url: "/doc" }));
 
 app.route("/auth", auth);
 app.route("/events", events);
