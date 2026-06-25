ca2552311e91f0a416052fdf0a37f7885d12f1a1
AmletixQ <nazarovamridin308@gmail.com>
2026-06-25 03:13:48 +0300

    feat: реализовал получение избранных и подписанных мероприятий

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,11 +1,14 @@
 import { hash } from "argon2";
 import { User } from "../generated/prisma/client";
 import { prisma } from "../lib";
+import { EventModel } from "../generated/prisma/models";
 
 interface UsersService {
   getAll(): Promise<User[]>;
   getById(id: string): Promise<User | null>;
   getByEmail(email: string): Promise<User | null>;
+  getFollowedEvents(userId: string): Promise<EventModel[]>;
+  getFavouritedEvents(userId: string): Promise<EventModel[]>;
 
   update(id: string, data: Partial<Omit<User, "id">>): Promise<User | null>;
   delete(id: string): Promise<User | null>;
@@ -22,6 +25,25 @@ export const usersService: UsersService = {
 
   async getByEmail(email) {
     return await prisma.user.findUnique({ where: { email } });
+  },
+
+  async getFollowedEvents(userId) {
+    const userEvents = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { followingEvents: true },
+    });
+    console.log(userEvents);
+
+    return userEvents?.followingEvents || [];
+  },
+
+  async getFavouritedEvents(userId) {
+    const userFavoriteEvents = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { favouritedEvents: true },
+    });
+
+    return userFavoriteEvents?.favouritedEvents || [];
   },
 
   async update(id, data) {
